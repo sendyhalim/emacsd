362d6f27b77660f4ec44d8998ffd769226cd797e
Sendy Halim <sendyhalim93@gmail.com>
2015-12-21 23:12:57 +0700

    Install simpleclip

--- init.el
+++ init.el
@@ -23,6 +23,9 @@
 (el-get-bundle neotree
   (global-set-key [f8] 'neotree-toggle))
 
+(el-get-bundle simpleclip
+  (simpleclip-mode 1))
+
 (el-get-bundle multiple-cursors)
 
 (el-get-bundle flycheck)
@@ -36,27 +39,11 @@
 (setq powerline-arrow-shape 'arrow)
 
 ;; ------------------------------------------
-;; Functions
-;; ------------------------------------------
-(defun paste-from-clipboard()
-  (interactive)
-  (setq x-select-enable-clipboard)
-  (yank)
-  (setq x-select-enable-clipboard nil))
-
-(defun copy-to-clipboard()
-  (interactive)
-  (setq x-select-enable-clipboard)
-  (kill-ring-save (region-beginning) (region-end))
-  (setq x-select-enable-clipboard nil))
-
-;; ------------------------------------------
 ;; General configs
 ;; ------------------------------------------
 (setq mac-option-modifier 'meta)
-(setq mac-command-modifier 'super)
-
 (setq backup-directory-alist `(("." . "~/.Emacs-backup-files")))
+
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
 (load-theme 'gruvbox t)
 
@@ -66,9 +53,6 @@
 ;; ------------------------------------------
 ;; Keybindings
 ;; ------------------------------------------
-(global-set-key (kbd "s-c") 'kill-ring-save)
-(global-set-key (kbd "s-v") 'yank)
-
 (define-key evil-normal-state-map (kbd "H") 'evil-beginning-of-line)
 (define-key evil-visual-state-map (kbd "H") 'evil-beginning-of-line)
 (define-key evil-normal-state-map (kbd "L") 'evil-end-of-line)
